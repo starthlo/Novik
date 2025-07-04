7d81380c9f29277bdcacb2518668765045084d07
starthlo <lkostyrka17@gmail.com>
2025-07-04 14:15:44 +0000

    fix: update vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,6 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr(), dts()],
-  // base: './',
   server: {
     port: 12000,
     strictPort: true,
@@ -16,19 +15,5 @@ export default defineConfig({
       '/api': 'http://127.0.0.1:8000',
     },
     allowedHosts: ['novik.ai', '127.0.0.1', 'dev1.infractura.com'],
-  },
-  build: {
-    target: ['es2015'],
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('react')) return 'react';
-          if (id.includes('react-dom')) return 'react-dom';
-          if (id.includes('react-router-dom')) return 'react-router';
-          if (id.includes('@mui') || id.includes('tailwind')) return 'ui';
-          if (id.includes('node_modules')) return 'vendor';
-        },
-      },
-    },
   },
 });
